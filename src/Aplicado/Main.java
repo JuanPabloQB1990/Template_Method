3c1898a05aaceab2ca7997c839ea74da1c0f8c40
Jhonatan Román Román <jhonro0507@gmail.com>
2023-09-01 06:59:37 -0500

    Corrijo el main del paquete Aplicado

--- src/Aplicado/Main.java
+++ src/Aplicado/Main.java
@@ -3,8 +3,6 @@ package Aplicado;
 public class Main {
     public static void main(String[] args) {
         AccesoProfesor profesor= new AccesoProfesor("1224", "Doc");
-        profesor.recopilarInformacion();
-        profesor.verificarIdentidad();
-        profesor.mostrarPaginaPrincipal();
+        profesor.validarInformacion();
     }
 }
